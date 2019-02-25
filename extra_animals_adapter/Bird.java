494bde9f20d22f44c61051ff5e4bcdce8a1f1dee
dorianvieira <vieira.dorian@outlook.fr>
2019-02-26 00:13:37 +0100

    add extra for adapter design pattern

---
+++ extra_animals_adapter/Bird.java
@@ -0,0 +1,10 @@
+// Java implementation of Adapter pattern 
+
+interface Bird 
+{ 
+	// birds implement Bird interface that allows 
+	// them to fly and make sounds adaptee interface 
+	public void fly(); 
+	public void makeSound(); 
+} 
+
---
+++ extra_animals_adapter/ToyDuck.java
@@ -0,0 +1,8 @@
+
+interface ToyDuck 
+{ 
+	// target interface 
+	// toyducks dont fly they just make 
+	// squeaking sound 
+	public void squeak(); 
+} 
